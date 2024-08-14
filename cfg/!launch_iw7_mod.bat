10b4e8301b84a311f26694dc46c08487c0fab225
Ahrimdon <mrcodblackops10>
2024-08-13 22:44:22 -0400

    maint: improve configs

--- cfg/!launch_iw7_mod.bat
+++ cfg/!launch_iw7_mod.bat
@@ -1,10 +1,9 @@
 @echo off
 
 set cfg="config_mp.cfg"
-set name="IW7 MP 1"
 set mod=
 
 set game_exe=iw7-mod.exe
 
-cd /D %cd%
+cd /D "%~dp0"
 start /B /wait /abovenormal %game_exe% +set onlinegame 1 +set fs_game "%mod%" -nosteam
--- cfg/!start_zm_server.bat
+++ cfg/!start_zm_server.bat
@@ -1,11 +1,11 @@
 @echo off
 ::///////////////////////////////////////////////////////////////////////
-::///  	         IW7-Mod Server Configuration start-up file	  		  ///
+:://             IW7-Mod Server Configuration startup file             //
 ::///////////////////////////////////////////////////////////////////////
 ::// This config best view with Notepad++ or other non-windows notepad //
 ::///////////////////////////////////////////////////////////////////////
-::// 	Your edited server_zm.cfg in the "main" folder goes here  	   //
-:://	This is were you edit your hostname, map & gamemodes from.     //
+:://        Your edited server_zm.cfg goes in the "main" folder        //
+:://     This is were you edit your hostname, map & gamemodes from     //
 ::///////////////////////////////////////////////////////////////////////
 
 set cfg=server_zm.cfg
@@ -14,7 +14,7 @@ set name="IW7 ZM 1"
 set mod=
 
 ::///////////////////////////////////////////////////////////////////////
-:://You're done!! WARNING!!! Don't mess with anything below this line  //
+:://  You're done! WARNING! Do not mess with anything below this line  //
 ::///////////////////////////////////////////////////////////////////////
 
 set game_exe=iw7-mod.exe
@@ -22,10 +22,10 @@ set game_exe=iw7-mod.exe
 title IW7 - %name% - Server restarter
 echo Server "%name%" will load %cfg% and listen on port %port% UDP!
 echo To shut down the server close this window first!
-echo (%date%)  -  (%time%) %name% server start.
+echo (%date%) - (%time%) %name% server start.
 
-cd /D %cd%
+cd /D "%~dp0"
 :server
-start /wait /abovenormal %game_exe% -dedicated +cpMode +set onlinegame 1 +set fs_game "%mod%" +set net_port "%port%" +exec %cfg% +map_rotate +set g_hardcore 0 +set xblive_privatematch 0
-echo (%date%)  -  (%time%) WARNING: %name% server closed or dropped... server restarts.
+start /wait /abovenormal %game_exe% -dedicated +cpMode +set onlinegame 1 +set fs_game "%mod%" +set net_port "%port%" +exec %cfg% +map_rotate +set g_hardcore 0 +set xblive_privatematch 0 +set sv_lanOnly 0
+echo (%date%) - (%time%) WARNING: %name% server closed or dropped... server restarts.
 goto server
