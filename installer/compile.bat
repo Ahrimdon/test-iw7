48a4f05e933e880438b28bf8db9dd64e3dbf21ba
Ahrimdon <mrcodblackops10>
2024-08-31 20:41:00 -0400

    maint: adjust installer compile script for vscode tasks + better error handling

--- installer/compile.bat
+++ installer/compile.bat
@@ -1,10 +1,20 @@
 @echo off
 
+cd /D "%~dp0"
+
 if not exist "build" mkdir "build"
+
 "tools\makensis.exe" "iw7_Installer.nsi"
+
+if %ERRORLEVEL% neq 0 (
+    echo.s
+    echo Compilation failed!
+    echo.
+    pause
+    exit /b %ERRORLEVEL%
+)
 
 echo.
 echo Compiled successfully!
 echo.
-
 pause
