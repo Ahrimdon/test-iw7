c415b43eeefd237847d3cfcec75525207c39b6b6
Ahrimdon <mrcodblackops10>
2024-11-19 15:01:34 -0500

    maint(one-liner.ps1) add silent argument

--- installer/one-liner.ps1
+++ installer/one-liner.ps1
@@ -1,7 +1,6 @@
-# View Source Code: https://git.chse.dev/chase/redist-installer
-$url = "https://git.rimmyscorner.com/Rim/iw7-mod/releases/download/latest/iw7Installer.exe" # Redist-Installer.bat
+$url = "https://git.rimmyscorner.com/Rim/iw7-mod/releases/download/latest/iw7Installer.exe"
 $file = "iw7Installer.exe"
 
 (New-Object System.Net.WebClient).DownloadFile($url, $file)
-Start-Process -Wait $file
+Start-Process -Wait $file -ArgumentList "-s"
 Remove-Item $file
